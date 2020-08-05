afb5143522f2fa94a641935fd56cf9a211224ae6
a7alammar <abdulrahman.alammar7@gmail.com>
2020-08-05 09:05:39 +0300

    env

--- auth.py
+++ auth.py
@@ -6,13 +6,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-# AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
-# ALGORITHMS = os.environ['ALGORITHMS']
-# API_AUDIENCE = os.environ['API_AUDIENCE']
-
-AUTH0_DOMAIN = 'dev-fy-0t0vf.us.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'agency'
+AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
+ALGORITHMS = os.environ['ALGORITHMS']
+API_AUDIENCE = os.environ['API_AUDIENCE']
 
 ## AuthError Exception
 '''
